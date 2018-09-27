ab1e435ac3daf3d8b8c4ba5f767333ccf0178751
DESKTOP-AGVU5J4\Javi <javiera@beerly.cl>
2018-09-26 23:08:49 -0300

    change class cards_home

--- page.php
+++ page.php
@@ -31,7 +31,7 @@
 				<div class="description_home">
 					<h3 class="sub_title"> Turismo</h3>
 					<p> listacdsfjshf</p>
-					<button class="btn btn-success">Ver Más</button>	
+					<button class="btn btn-success btn__cards">Ver Más</button>		
 				</div>
 				
 		    </div>
@@ -41,7 +41,7 @@
 				</div>
 				<h3 class="sub_title"> Ubicación</h3>
 				<p> listacdsfjshf</p>
-				<button class="btn btn-success">Ver Más</button>
+				<button class="btn btn-success btn__cards">Ver Más</button>	
 		    </div>
 		  </div>
 		</div>
@@ -59,32 +59,31 @@
 		    </div>
 		  </div>
 		</div>
-		<div class="medio">
-			<div class="col-lg-6 col-xs-12 form_contact">
-				Aqui va el mapa de google
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-xs-12 form_contact">
+					Aqui va el mapa de google
+				</div>
+				<div class="col-lg-6 col-xs-12 form_contact">
+					<form>
+					  <div class="form-group">
+					    <label for="exampleInputEmail1">Email address</label>
+					    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+					    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+					  </div>
+					  <div class="form-group">
+					    <label for="exampleInputPassword1">Password</label>
+					    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+					  </div>
+					  <div class="form-check">
+					    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+					    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+					  </div>
+					  <button type="submit" class="btn btn-primary">Submit</button>
+					</form>	
+				</div>		
 			</div>
-			<div class="col-lg-6 col-xs-12 form_contact">
-				<form>
-				  <div class="form-group">
-				    <label for="exampleInputEmail1">Email address</label>
-				    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-				    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-				  </div>
-				  <div class="form-group">
-				    <label for="exampleInputPassword1">Password</label>
-				    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-				  </div>
-				  <div class="form-check">
-				    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-				    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-				  </div>
-				  <button type="submit" class="btn btn-primary">Submit</button>
-				</form>	
-			</div>		
-		</div>
-		<div class="row">
-		  <div class="col-xs-6">.col-xs-6</div>
-		  <div class="col-xs-6">.col-xs-6</div>
+			
 		</div>
 
 <?php get_sidebar() ?>
