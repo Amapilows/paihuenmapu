dd46603517f1c5dfef74d8759b5c8167b0de9ddf
DESKTOP-AGVU5J4\Javi <javiera@beerly.cl>
2018-09-30 12:12:19 -0300

    cambios en footer y header

--- footer.php
+++ footer.php
@@ -1,23 +1,11 @@
-<?php if ( has_nav_menu( 'footer-menu' ) ) { ?>
-	<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'header_menu' ) ); ?>
-<?php } ?>
 
-<?php wp_footer() ?>
 	<footer>
 		<div class="footer">
-			<ul>
-				<li> <a> Inicio | </a></li>
-				<li> <a> Cabañas | </a></li>
-				<li> <a> Servicios | </a></li>
-				<li> <a>FAQ | </a></li>
-				<li> <a> Blog | </a></li>
-				<li> <a> Contacto </a></li>
-			</ul>
-			<div>
-				<i class="fab fa-facebook-square"></i>
-				<i class="fab fa-instagram"></i>				
-			</div>
+			<?php if ( has_nav_menu( 'footer-menu' ) ) { ?>
+			<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'footer-menu' ) ); ?>
+			<?php } ?>
 
+			<?php wp_footer() ?>
 
 		</div>
 	</footer>
--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
 	<meta charset="<?php bloginfo('charset') ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,15 +10,14 @@
 	<?php wp_head() ?>
 </head>
 <body>
-	<header class="navbar navbar-expand flex-column flex-md-row bd-navbar fixed-top">
-		<nav class="navbar navbar-expand-lg ">
-			<div>
-				<a class="navbar-brand" href="#">
-    				<img src="<?php echo get_template_directory_uri(); ?>/src/images/logo.png" class="logo">
-  		  		</a>
-			</div>
-		</nav>		
-	</header>
-<?php if ( has_nav_menu( 'header-menu' ) ) { ?>
-	<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'header_menu' ) ); ?>
-<?php } ?>
+<nav class="navbar navbar-expand flex-column flex-md-row bd-navbar fixed-top">
+  <div class="navbar navbar-expand-lg ">
+    <a class="navbar-brand" href="#">
+        <img src="<?php echo get_template_directory_uri(); ?>/src/images/logo.png" class="logo">
+        </a>
+  <div>
+  <?php if ( has_nav_menu( 'header-menu' ) ) { ?>
+
+  <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'header_menu' ) ); ?>
+  <?php } ?>
+</nav>
