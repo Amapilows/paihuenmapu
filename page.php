fd92ed589d46b1e9f36fe76cd463ba93106e7cc4
DESKTOP-AGVU5J4\Javi <javiera@beerly.cl>
2018-10-04 11:48:30 -0300

    cambios

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
 
 	<div class="page_style">
 		<div class="contenido-hero" style="background-image: url("<?php get_the_post_thumbnail_url(); ?>");"
-			<div class="textto-hero">
+			<div class="textto-hero col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-12">
 
 				<?php the_post() ?>
 				<?php the_title() ?>
