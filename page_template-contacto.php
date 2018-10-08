ae3398b196c84482b999937fed3becf746af3d3e
DESKTOP-AGVU5J4\Javi <javiera@beerly.cl>
2018-10-07 23:53:15 -0300

    contact

--- page_template-contacto.php
+++ page_template-contacto.php
@@ -1,22 +1,25 @@
 <?php /* Template Name: Contacto */ ?>
 <?php get_header() ?>
 	<div class="container-contact">
+		<h1 class="title--contact"><?php the_title() ?></h1>	
 		<img src="<?php echo get_field('banner')['url']; ?>" alt="" class="banner-contact">
-		<h1><?php the_title() ?></h1>		
 	</div>
 
-	<div class="container">
-		<div class="row">
-			<div class="contact--form">
-				<form action="">
-					<label for="">Nombre</label>
-					<input type="" name="Coloca tu nombre">
-
-				</form>
-				
-			</div>			
-		</div>
-		
-	</div>
+	<form>
+	  <div class="form-group">
+	    <label for="exampleInputEmail1">Email address</label>
+	    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+	    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+	  </div>
+	  <div class="form-group">
+	    <label for="exampleInputPassword1">Password</label>
+	    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+	  </div>
+	  <div class="form-check">
+	    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+	    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+	  </div>
+	  <button type="submit" class="btn btn-primary">Submit</button>
+	</form>
 
 <?php get_footer()  ?>	
